49f42bb484ac6c6be17ee5d1a6124993de2b0a2b
Sierra Saitta-Zelterman <ssaitta13@gmail.com>
2017-11-06 11:41:07 -0500

    ls complete

--- bash.js
+++ bash.js
@@ -1,31 +1,13 @@
+
+var commands = require('./commands.js');
+
 process.stdout.write('prompt > ');
-
-// The stdin 'data' event fires after a user types in a line
-
-
 
 process.stdin.on('data', function (data) {
     var cmd = data.toString().trim(); // remove the newline
-    var output;
-    if (cmd === "pwd") {
-        output = commands.pwd();
-        // output = __dirname;
-    } else if (cmd === "date") {
-        output = commands.date();
-        // let datetime = new Date();
-        // output = datetime.toString();
-    }
+    commands[cmd]();
 
-
-    process.stdout.write(output);
-    process.stdout.write('\nprompt > ');
+    //process.stdout.write('\nprompt > ');
   
   });
 
-
-
-// require("./fileb.js")
-var commands = require('./commands.js');
-// console.log(commands);
-
-commands.pwd;
--- commands.js
+++ commands.js
@@ -1,12 +1,28 @@
+var fs = require('fs');
+
 exports.pwd = function() {
-  // process.stdout.write(__dirname);
-  return __dirname.toString();
+  process.stdout.write(__dirname + "\n");
+  process.stdout.write("prompt > ");
+
 }
 
 exports.date = function() {
   let datetime = new Date();
   output = datetime.toString();
-  return output;
 
-  // process.stdout.write(output);
+
+  process.stdout.write(output + "\n");
+  process.stdout.write("prompt > ");
 }
+
+exports.ls = function(){
+  fs.readdir('.', function(err, files) {
+    if (err) throw err;
+    files.forEach(function(file) {
+      process.stdout.write(file.toString() + "\n");
+    })
+    process.stdout.write("prompt > ");
+  });
+  //process.stdout.write()
+}
+
