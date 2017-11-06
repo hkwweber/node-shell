7e81ee92d6519b2d893f57ea639185064e1b2cdb
Ann Layman <annelayman@gmail.com>
2017-11-06 11:21:21 -0500

    correctly required and exported

--- bash.js
+++ bash.js
@@ -4,9 +4,28 @@ process.stdout.write('prompt > ');
 
 
 
+process.stdin.on('data', function (data) {
+    var cmd = data.toString().trim(); // remove the newline
+    var output;
+    if (cmd === "pwd") {
+        output = commands.pwd();
+        // output = __dirname;
+    } else if (cmd === "date") {
+        output = commands.date();
+        // let datetime = new Date();
+        // output = datetime.toString();
+    }
+
+
+    process.stdout.write(output);
+    process.stdout.write('\nprompt > ');
+  
+  });
+
 
 
 // require("./fileb.js")
 var commands = require('./commands.js');
 // console.log(commands);
 
+commands.pwd;
--- commands.js
+++ commands.js
@@ -1,27 +1,12 @@
-process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
-  var output;
-  if (cmd === "pwd") {
-      output = __dirname;
-  } else if (cmd === "date") {
-      let datetime = new Date();
-      output = datetime.toString();
-  }
-
-   process.stdout.write(output);
-  process.stdout.write('\nprompt > ');
-
-});
-
 exports.pwd = function() {
-  process.stdout.write(__dirname);
-
+  // process.stdout.write(__dirname);
+  return __dirname.toString();
 }
 
 exports.date = function() {
   let datetime = new Date();
   output = datetime.toString();
+  return output;
 
-
-  process.stdout.write(output);
+  // process.stdout.write(output);
 }
