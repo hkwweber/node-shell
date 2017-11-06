86d862c47dc6b866b69be78c953cd08d7e54a537
Ann Layman <annelayman@gmail.com>
2017-11-06 10:25:32 -0500

    pwd working

---
+++ bash.js
@@ -0,0 +1,17 @@
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+  var cmd = data.toString().trim(); // remove the newline
+  var output;
+  if (cmd === "pwd") {
+      output = __dirname;
+  } else if (cmd === "date") {
+      output = Date.now();
+  }
+
+
+  process.stdout.write(output);
+  process.stdout.write('\nprompt > ');
+
+});
